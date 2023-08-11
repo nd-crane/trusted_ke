4d35edbb913593993240d4bccd64fdab0b65569c
JonathanKarr33 <jka83321@gmail.com>
2023-08-11 17:15:04 -0400

    spacey coreference working, output still needed

--- coref/coref.py
+++ coref/coref.py
@@ -1,51 +1,24 @@
 #!/usr/bin/env python3
-
-import crosslingual_coreference
 import spacy
 import pandas as pd
+from pathlib import Path
+
 
 def resolve():
-
-    DEVICE = 0 # Number of the GPU, -1 if want to use CPU
-
-    #Disabled pipes will be loaded but they won’t be run unless you explicitly enable them
-    #using en_core_web_trf instead of en_core_web_sm for increased accuracy
-    coref = spacy.load('en_core_web_trf', disable=['ner', 'tagger', 'parser', 'attribute_ruler', 'lemmatizer'])
-
-    # Add coreference resolution model as shown in documentation
-    #https://spacy.io/universe/project/crosslingualcoreference
-    coref.add_pipe("xx_coref", config={"chunk_size": 2500, "chunk_overlap": 2, "device": DEVICE})
-
-
-    # apply to MaintNet on text cols
+    coref = spacy.load('en_core_web_trf')
     df = pd.read_csv('MaintNet_data/Aircraft_Annotation_DataFile.csv')
-    #textcols = ['problemDescription','csmpNarrativeSummary','recommendSolution']
-    textcols = ['problemDescription']
 
-    corefdict = {textcol : [] for textcol in textcols}
-    for irow in range(len(df)):
-        for textcol in textcols:
-            if type(df[textcol].iat[irow]) == str:
-                if len(df[textcol].iat[irow].split()) > 2:
-                    try:
-                        corefdict[textcol].append(coref(df[textcol].iat[irow])._.resolved_text)
-                    except:
-                        print(textcol, irow)
-                        print(df[textcol].iat[irow])
-                        print(coref(df[textcol].iat[irow]))
-                        print(coref(df[textcol].iat[irow])._.resolved_text)
-                        print(corefdict[textcol])
-                    
-                else:
-                    corefdict[textcol].append(df[textcol].iat[irow])
-                    
-            else:
-                corefdict[textcol].append('')
-                
-    pd.DataFrame.from_dict(corefdict).to_csv('MaintNet_data/Aircraft_Annotation_DataFile.csv', index=False)
-    
+    # Note using first 100 col temporary so it doesn't run long
+    new_df = pd.DataFrame(df['IDENT'][:100], columns=['IDENT'])
+    textcols = ['PROBLEM', 'ACTION']
+    for textcol in textcols:
+        new_df[textcol] = df[textcol][:100].apply(coref)
+    filepath = Path('models/Aircraft_Annotation_DataModel.csv')
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    new_df.to_csv(filepath)
+    # TODO: fix issue why it's outputting the df not new_df file
     return
 
-    
+
 if __name__ == '__main__':
     resolve()
