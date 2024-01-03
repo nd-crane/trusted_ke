f9422da1fb914127e474e110c6e475e6acea0c46
kpmealey <kmealey2@nd.edu>
2024-01-03 12:25:42 -0500

    Blink

---
+++ nel/blink/annotate_faa.py
@@ -0,0 +1,59 @@
+import sys
+sys.path.append('./BLINK')
+from blink import ner as NER
+import json
+import pandas as pd
+
+## Copied from main_dense.py, added idoc to arguments and line 26
+def _annotate(ner_model, input_sentences, idoc):
+    ner_output_data = ner_model.predict(input_sentences)
+    sentences = ner_output_data["sentences"]
+    mentions = ner_output_data["mentions"]
+    samples = []
+    for mention in mentions:
+        record = {}
+        record["label"] = "unknown"
+        record["label_id"] = -1
+        # LOWERCASE EVERYTHING !
+        record["context_left"] = sentences[mention["sent_idx"]][
+            : mention["start_pos"]
+        ].lower()
+        record["context_right"] = sentences[mention["sent_idx"]][
+            mention["end_pos"] :
+        ].lower()
+        record["mention"] = mention["text"].lower()
+        record["start_pos"] = int(mention["start_pos"])
+        record["end_pos"] = int(mention["end_pos"])
+        record["sent_idx"] = mention["sent_idx"]
+        record["doc_idx"] = idoc
+        samples.append(record)
+    return samples
+
+if __name__=="__main__":
+    
+    data = pd.read_csv("../../data/FAA_data/Maintenance_Text_data_nona.csv")
+    text = list(data["c119"])
+    
+    # initialize jsonl_output
+    jsonl_output = []
+            
+    # Load NER model
+    ner_model = NER.get_model()
+    
+    for idoc, doc in enumerate(text):
+        
+        # Split into seperate sentences if necessary
+        if '.' in doc:
+            sentences = [sentence + '.' for sentence in doc.split('.')]
+        else:
+            sentences = [doc]
+            
+        # annotate
+        samples = _annotate(ner_model, sentences, idoc)
+        
+        jsonl_output.extend(samples)
+    
+    with open("./faa_samples.jsonl", "w") as json_file:
+        json_file.write(json.dumps(jsonl_output[0]))
+        for line in jsonl_output[1:]:
+            json_file.write('\n'+json.dumps(line))
---
+++ nel/blink/run_blink.py
@@ -0,0 +1,127 @@
+import pandas as pd
+import argparse
+import json
+import re
+import requests
+import sys
+sys.path.append("./BLINK")
+import blink.candidate_ranking.utils as utils
+from main_dense import load_models, run
+
+def lookup(url, qiddict):
+    
+    qidp = re.compile(r'.*id="t-wikibase" class="mw-list-item"><a href="https://www.wikidata.org/wiki/Special:EntityPage/(Q\d*)".*')
+
+    content = str(requests.get(url).content)
+    mo = re.match(qidp, content)
+
+    if mo:
+        QID = mo.groups()[0]
+    else:
+        QID = url
+        
+    qiddict.update({url:QID})
+            
+    return QID, qiddict
+
+models_path = "BLINK/models/" # the path where you stored the BLINK models
+annotated_samples_path = "./faa_samples.jsonl"
+
+config = {
+    "test_entities": None,
+    "test_mentions": annotated_samples_path,
+    "interactive": False,
+    "top_k": 10,
+    "biencoder_model": models_path+"biencoder_wiki_large.bin",
+    "biencoder_config": models_path+"biencoder_wiki_large.json",
+    "entity_catalogue": models_path+"entity.jsonl",
+    "entity_encoding": models_path+"all_entities_large.t7",
+    "crossencoder_model": models_path+"crossencoder_wiki_large.bin",
+    "crossencoder_config": models_path+"crossencoder_wiki_large.json",
+    "fast": False,
+    "output_path": "./BLINK/logs/" # logging directory
+}
+
+args = argparse.Namespace(**config)
+
+logger = utils.get_logger(args.output_path)
+
+models = load_models(args, logger)
+
+title2id = models[5]
+id2title = models[6]
+id2text = models[7]
+wikipedia_id2local_id = models[8]
+
+id2url = {
+        v: "https://en.wikipedia.org/wiki?curid=%s" % k
+        for k, v in wikipedia_id2local_id.items()
+    }
+
+# Get test data
+with open("./faa_samples.jsonl") as f:
+    jsonl_data = f.read()
+
+samples = []
+for json_str in jsonl_data.split('\n'):
+    try:
+        samples.append(json.loads(json_str))
+    except:
+        pass
+
+## Crossencoder predictions
+result = run(args, logger, *models, test_data=samples)
+crossencoder_predictions = result[5]
+
+
+## Biencoder predictions -- change fast to True
+args.fast = True
+result = run(args, logger, *models, test_data=samples)
+biencoder_predictions = result[5]
+
+## Store results
+og_data = pd.read_csv("../../data/FAA_data/Maintenance_Text_data_nona.csv")
+
+results_dict = {"doc_idx":[], "sent_idx":[], "original_sentence":[], "input":[], "mention":[], "bi_pred_entity":[], "bi_qid":[], "bi_desc":[], "cross_pred_entity":[], "cross_qid":[],"cross_desc":[]}
+
+url2qid = {} # stores each looked-up qid for faster referencing later
+
+for isample, sample in enumerate(samples):
+    
+    results_dict["doc_idx"].append(sample["doc_idx"])
+    results_dict["sent_idx"].append(sample["sent_idx"])
+    results_dict["original_sentence"].append(og_data["c119"].iat[sample["doc_idx"]])
+    results_dict["input"].append(''.join([sample["context_left"], sample["mention"], sample["context_right"]]))
+    results_dict["mention"].append(sample["mention"])
+    
+    # Biencoder
+    entity = biencoder_predictions[isample][0] # just taking top one
+    entity_id = title2id[entity]
+    url = id2url[entity_id]
+    desc = id2text[entity_id]
+
+    if url in url2qid:
+        qid = url2qid[url]
+    else:
+        qid, url2qid = lookup(url, url2qid)
+
+    results_dict["bi_pred_entity"].append(entity)
+    results_dict["bi_qid"].append(qid)
+    results_dict["bi_desc"].append(desc)
+
+    # Crossencoder:
+    entity = crossencoder_predictions[isample][0]
+    entity_id = title2id[entity]
+    url = id2url[entity_id]
+    desc = id2text[entity_id]
+
+    if url in url2qid:
+        qid = url2qid[url]
+    else:
+        qid, url2qid = lookup(url, url2qid)
+
+    results_dict["cross_pred_entity"].append(entity)
+    results_dict["cross_qid"].append(qid)
+    results_dict["cross_desc"].append(desc)
+    
+pd.DataFrame(results_dict).to_csv("../../data/results/blink_results.csv", index=False)
