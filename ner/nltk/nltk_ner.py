ec2af9a97802e8c41a150c7930372a7c554bed4f
kpmealey <kmealey2@nd.edu>
2024-02-22 01:27:07 -0500

    fixed indexing

--- ner/nltk/nltk_ner.py
+++ ner/nltk/nltk_ner.py
@@ -15,6 +15,32 @@ out_dict = {'index':[], 'c5_unique_id':[], 'c119_text':[], "entities":[], "POS t
 
 for index in range(len(sample)):
     
+    text = sample["c119"][index]
+    
+    tokens = nltk.word_tokenize(text)
+    tagged_tokens = nltk.pos_tag(tokens)
+    chunked = nltk.ne_chunk(tagged_tokens)
+    entities = []
+    for tree in chunked.subtrees():
+        if tree.label() == "S":
+            continue
+        out_dict["index"].append(sample['Unnamed: 0'][index])
+        out_dict["c5_unique_id"].append(sample["c5"][index])
+        out_dict["c119_text"].append(text)
+        out_dict["entities"].append(' '.join([pair[0] for pair in tree.leaves()]))
+        out_dict["POS tags"].append(', '.join([pair[1] for pair in tree.leaves()]))
+        out_dict["labels"].append(tree.label())
+
+print(pd.DataFrame(out_dict))
+
+pd.DataFrame(out_dict).to_excel("nltk_ner_uppercased.xlsx", index=False)
+
+###
+
+out_dict = {'index':[], 'c5_unique_id':[], 'c119_text':[], "entities":[], "POS tags":[], "labels":[]}
+
+for index in range(len(sample)):
+    
     text = sample["c119"][index].lower()
     
     tokens = nltk.word_tokenize(text)
@@ -24,7 +50,7 @@ for index in range(len(sample)):
     for tree in chunked.subtrees():
         if tree.label() == "S":
             continue
-        out_dict["index"].append(index)
+        out_dict["index"].append(sample['Unnamed: 0'][index])
         out_dict["c5_unique_id"].append(sample["c5"][index])
         out_dict["c119_text"].append(text)
         out_dict["entities"].append(' '.join([pair[0] for pair in tree.leaves()]))
