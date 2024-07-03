e8942bf417e3d26e09bbaad55c0bb152b76f5766
kpmealey <kmealey2@nd.edu>
2024-07-02 21:25:03 -0400

    added strict evaluation to NER

--- evaluations/automatic/ner_eval.py
+++ evaluations/automatic/ner_eval.py
@@ -13,7 +13,7 @@ def print_results(tool_name, precision, recall, f1_score):
 
     return
 
-def calculate_precision_recall_f1(gs, df_tool, id_col, ent_col):
+def calculate_precision_recall_f1(gs, df_tool, id_col, ent_col, strict):
     """
     Calculate precision and recall based on entities comparison between gs (ground truth) and df_tool (answers).
     
@@ -31,20 +31,33 @@ def calculate_precision_recall_f1(gs, df_tool, id_col, ent_col):
     # Check for True Positives and False Negatives by iterating over gs
     for index, gs_row in gs.iterrows():
         gs_id, gs_entity = gs_row['id'], gs_row[ent_col]
-        tool_entities = [entity.upper() for entity in df_tool.loc[df_tool[id_col] == gs_id, ent_col].tolist()]
-        
-        if any(gs_entity in tool_entity or tool_entity in gs_entity for tool_entity in tool_entities):
-            TP += 1
+        tool_entities = [entity.upper() for entity in df_tool.loc[df_tool[id_col] == gs_id, ent_col].tolist()] # get all the entities the tool generated for the gs_id entry
+
+        # In strict matching, we only count a tool-generated entity as correct if it exactly matches the gold standard entity
+        if strict:
+            if gs_entity in tool_entities:
+                TP += 1
+            else:
+                FN += 1
+        # In non-strict matching, we count a tool-generated entity as correct if it is a subspan of the gold standard entity, or if the gold standard entity is a subspan of it
         else:
-            FN += 1
+            if any(gs_entity in tool_entity or tool_entity in gs_entity for tool_entity in tool_entities):
+                TP += 1
+            else:
+                FN += 1
     
     # Check for False Positives by iterating over df_tool
     for index, tool_row in df_tool.iterrows():
         tool_id, tool_entity = tool_row[id_col], tool_row[ent_col]
         gs_entities = gs.loc[gs['id'] == tool_id, ent_col].tolist()
-        
-        if not any(tool_entity in gs_entity or gs_entity in tool_entity for gs_entity in gs_entities):
-            FP += 1
+
+        #  Strict matching
+        if strict:
+            if tool_entity not in gs_entities:
+                FP += 1
+        else:
+            if not any(tool_entity in gs_entity or gs_entity in tool_entity for gs_entity in gs_entities):
+                FP += 1
     
     # Calculate precision and recall
     precision = TP / (TP + FP) if (TP + FP) > 0 else 0
@@ -55,14 +68,14 @@ def calculate_precision_recall_f1(gs, df_tool, id_col, ent_col):
     
     return precision, recall, f1_score
 
-def eval(dataset_path, gs_path, id_col, ent_col):
+def eval(dataset_path, gs_path, id_col, ent_col, strict):
 
     gs = pd.read_csv(gs_path)
     df_tool = pd.read_csv(dataset_path)
 
     tool_name = dataset_path.split('/')[-2]
 
-    precision, recall, f1_score = calculate_precision_recall_f1(gs, df_tool, id_col, ent_col)
+    precision, recall, f1_score = calculate_precision_recall_f1(gs, df_tool, id_col, ent_col, strict)
 
     print_results(tool_name, precision, recall, f1_score)
 
@@ -98,8 +111,15 @@ if __name__=='__main__':
         default="../../gold_standard/processed/ner.csv",
         help='Path to NER gold standard'
     )
+    parser.add_argument(
+        '-s', '--strict',
+        required=False,
+        action='store_true',
+        default=False,
+        help='Whether or not to perform strict-match evaluation'
+    )
 
     args = parser.parse_args()
 
     # call eval
-    eval(args.dataset_path, args.gs_path, args.id_col, args.ent_col)
+    eval(args.dataset_path, args.gs_path, args.id_col, args.ent_col, args.strict)
