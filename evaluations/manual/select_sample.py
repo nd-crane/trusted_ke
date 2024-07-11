92b5a49a04a82b04aa845c59be9ee7a67bdf1680
kpmealey <kmealey2@nd.edu>
2024-07-11 13:52:11 -0400

    updated sample script to save multiple sheets and use indices

--- evaluations/manual/select_sample.py
+++ evaluations/manual/select_sample.py
@@ -3,12 +3,12 @@
 import pandas as pd
 import argparse
 
-def select_samples(result_df, sample_ids, id_col):
+def select_samples(result_df, samples, id_col):
 
     sampled_result_df = pd.DataFrame(columns=result_df.columns)
-    for sample_id in sample_ids:
-        id_rows = result_df[result_df[id_col]==sample_id]
-        sampled_result_df = pd.concat([sampled_result_df, id_rows], ignore_index=True)
+    for sample_no in samples:
+        rows = result_df[result_df[id_col]==sample_no]
+        sampled_result_df = pd.concat([sampled_result_df, rows], ignore_index=True)
     
     return sampled_result_df
 
@@ -30,6 +30,18 @@ if __name__=='__main__':
         help='Name of column in input dataset which contains unique identifier'
     )
     parser.add_argument(
+        '-n', '--index_col',
+        type=str,
+        required=False,
+        default="index",
+        help='Name of column in input dataset which contains the index from Maintenance_Text_data_nona.csv'
+    )
+    parser.add_argument(
+        '--use_indices',
+        action='store_true',
+        help='If true, it uses indices instead of the c5_id to select the samples'
+    )
+    parser.add_argument(
         '-s', '--sample_path',
         type=str,
         required=False,
@@ -49,8 +61,16 @@ if __name__=='__main__':
     result_df = pd.read_csv(args.dataset_path)
     sample_df = pd.read_csv(args.sample_path)
 
-    sample_ids = list(sample_df['c5'])
+    if args.use_indices:
+        samples = list(sample_df['Unnamed: 0'])
+        id_col = args.index_col
+    else:
+        samples = list(sample_df['c5'])
+        id_col = args.id_col
 
-    sampled_result_df = select_samples(result_df, sample_ids, args.id_col)
+    sampled_result_df = select_samples(result_df, samples, id_col)
 
-    sampled_result_df.to_excel(args.output_folder + args.dataset_path.split('/')[-1].split('.csv')[0]+'_eval.xlsx', index=False)
+    file_name = args.output_folder + args.dataset_path.split('/')[-1].split('.csv')[0]+'_eval.xlsx'
+    with pd.ExcelWriter(file_name) as writer:
+        sampled_result_df.to_excel(writer, sheet_name='sampled', index=False)
+        result_df.to_excel(writer, sheet_name='raw_output', index=False)
