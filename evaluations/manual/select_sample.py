1d6285711593e80dcc08a57967df6e50c3b38dc4
kpmealey <kmealey2@nd.edu>
2024-07-11 13:31:18 -0400

    select_sample script for eval, unirel sampled excel

---
+++ evaluations/manual/select_sample.py
@@ -0,0 +1,56 @@
+# This script selects the 100 records used for eval from a result in data/results/tool_name, so that it can be downloaded and evaluated manually.
+
+import pandas as pd
+import argparse
+
+def select_samples(result_df, sample_ids, id_col):
+
+    sampled_result_df = pd.DataFrame(columns=result_df.columns)
+    for sample_id in sample_ids:
+        id_rows = result_df[result_df[id_col]==sample_id]
+        sampled_result_df = pd.concat([sampled_result_df, id_rows], ignore_index=True)
+    
+    return sampled_result_df
+
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        '-d', '--dataset_path',
+        type=str,
+        required=True,
+        help='path/to/results/dataset.csv'
+    )
+    parser.add_argument(
+        '-i', '--id_col',
+        type=str,
+        required=False,
+        default="c5_id",
+        help='Name of column in input dataset which contains unique identifier'
+    )
+    parser.add_argument(
+        '-s', '--sample_path',
+        type=str,
+        required=False,
+        default="../../data/sampling/FAA_sample_100.csv",
+        help='Path to sample data'
+    )
+    parser.add_argument(
+        '-o', '--output_folder',
+        type=str,
+        required=False,
+        default="./",
+        help='Folder in which to save output'
+    )
+
+    args = parser.parse_args()
+
+    result_df = pd.read_csv(args.dataset_path)
+    sample_df = pd.read_csv(args.sample_path)
+
+    sample_ids = list(sample_df['c5'])
+
+    sampled_result_df = select_samples(result_df, sample_ids, args.id_col)
+
+    sampled_result_df.to_excel(args.output_folder + args.dataset_path.split('/')[-1].split('.csv')[0]+'_eval.xlsx', index=False)
